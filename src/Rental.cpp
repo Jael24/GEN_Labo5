f3acb7fe818e039363256da717a1ffac9e5a1def
Jael Dubey <jael.dubey@heig-vd.ch>
2019-06-16 13:50:50 +0200

    Refactoring 1 : suppression des méthodes inline de Rental.h et création du .cpp

--- src/Rental.cpp
+++ src/Rental.cpp
@@ -1,4 +1,12 @@
-//
-// Created by jaeld on 16.06.2019.
-//
+#include "Rental.h"
 
+Rental::Rental( const Movie& movie, int daysRented )
+: _movie( movie ) , _daysRented( daysRented ) {}
+
+int Rental::getDaysRented() const {
+    return _daysRented;
+}
+
+const Movie& Rental::getMovie() const {
+    return _movie;
+}
